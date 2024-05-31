3874bf5d7e333ebcc2c5c0425ea4ef5e1249cc5d
alexanderhernandez17 <alexander.hdez.2001@gmail.com>
2024-05-31 14:18:42 -0400

    persistence with vercel/postgre fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthorModule } from './author/author.module';
-import { ClientModule } from './client/client.module';
-import { AuthorModule } from './author/author.module';
-import { SaleModule } from './sale/sale.module';
-import { BookModule } from './book/book.module';
-import { AuthorModule } from './author/author.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from '@nestjs/config';
+import { AuthorModule } from "./author/author.module";
+import { BookModule } from "./book/book.module";
+import { SaleModule } from "./sale/sale.module";
+import { ClientModule } from "./client/client.module";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [AuthorModule, BookModule, SaleModule, ClientModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+      "ssl": true
+    }),
+    AuthorModule, BookModule, SaleModule, ClientModule],
   controllers: [AppController],
   providers: [AppService],
 })
