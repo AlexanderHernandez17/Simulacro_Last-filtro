9a3f3ca7e183ea18e1b9e918852777d909dc32e1
alexanderhernandez17 <alexander.hdez.2001@gmail.com>
2024-05-31 13:09:28 -0400

    entities added

--- src/author/entities/author.entity.ts
+++ src/author/entities/author.entity.ts
@@ -1,1 +1,15 @@
-export class Author {}
+import { Book } from 'src/book/entities/book.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+
+@Entity()
+export class Author {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToMany(() => Book, (book) => book.authors)
+  books: Book[];
+}
+
--- src/sale/entities/sale.entity.ts
+++ src/sale/entities/sale.entity.ts
@@ -1,1 +1,18 @@
-export class Sale {}
+import { Book } from 'src/book/entities/book.entity';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+
+
+@Entity()
+export class Sale {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Book)
+  book: Book;
+
+  @Column()
+  client: string;
+
+  @Column()
+  saleDate: Date;
+}
